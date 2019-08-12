9ff0b9b4d890170f384046bf7b1b60c6eacba8c1
Susan Renaud <serenaud89@gmail.com>
2019-08-12 09:02:49 -0500

    completed stupid coaching exercise

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question =~ /I am going to work/i
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don’t care, get dressed and go to work!'
+    end
+  end
+end
